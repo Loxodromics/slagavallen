a19f7e1b965c2bf43b75a0e816be38f9d5b68c41
Philipp Engelhard <philippengelhard@gmail.com>
2020-11-02 17:33:39 +0100

    missing Tile files

---
+++ src/logic/tile.cpp
@@ -0,0 +1,22 @@
+//
+//  tile.cpp
+//  slagavallen
+//
+//  Created by philipp on 02.11.2020.
+//  Copyright (c) 2020 Philipp Engelhard. All rights reserved.
+//
+#include "tile.h"
+
+namespace LFD {
+
+namespace slagavallen {
+
+Tile::Tile()
+{
+
+}
+
+
+}	// namespace slagavallen
+
+}	// namespace LFD
---
+++ src/logic/tile.h
@@ -0,0 +1,44 @@
+//
+//  tile.h
+//  slagavallen
+//
+//  Created by philipp on 02.11.2020.
+//  Copyright (c) 2020 Philipp Engelhard. All rights reserved.
+//
+#pragma once
+
+namespace LFD {
+
+namespace slagavallen {
+
+class Tile
+{
+public:
+
+	enum TerrainType {
+		None,
+		Grass0,
+		Grass1,
+		Grass2,
+		Grass3,
+	};
+
+	enum Rotation {
+		Rot_0,
+		Rot_90,
+		Rot_180,
+		Rot_270,
+	};
+
+	explicit Tile();
+
+	unsigned int id;
+	TerrainType terrainType;
+	Rotation rotation;
+
+};
+
+}	// namespace slagavallen
+
+}	// namespace LFD
+
